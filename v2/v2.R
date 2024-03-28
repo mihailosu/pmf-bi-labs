aea8af5cec075eb3bca1c35723ef7e8a18814bc7
Mihailo Ilic <milic@dmi.uns.ac.rs>
2024-03-28 20:06:54 +0100

    v2 finished

--- v2/v2.R
+++ v2/v2.R
@@ -38,7 +38,36 @@ nrow(data)
 
 data <- data[!duplicated(data$ID),]
 
+# Konverzija u faktore
 
+summary(data) # Kolona neighborhood je klase character
+
+data$neighborhood <- as.factor(data$neighborhood)
+data$city <- as.factor(data$city)
+data$flooring_type <- as.factor(data$flooring_type)
+
+class(data$neighborhood)
+summary(data) # Kolona neighborhood je sada klase factor i korektno prikazuje kategorije delova grada
+
+
+# Osnovne desktiptive funkcije, mean, median, max, min ...
+mean(data$price)
+
+median(data$price)
+
+var(data$price)
+
+sd(data$price)
+
+sqrt(var(data$price))
+
+min(data$price)
+
+max(data$price)
+
+range(data$price)
+
+cor(data$price, data$square_feet, method = "pearson")
 
 # Brz pregled i ukrstanje kolona moze se izvesti pozivom komande table
 
@@ -46,4 +75,49 @@ table(data$balcony)
 
 # Moguce je ukrstiti 2 kolone
 
-table(data$balcony, data$)
+table(data$balcony, data$neighborhood)
+
+# Procentualni prikaz
+
+table(data$balcony) / length(data$balcony)
+
+t1 = table(data$balcony, data$neighborhood)
+
+prop.table(t1, 2)
+
+# Osnovno crtanje
+
+hist(data$price)
+
+# Histogram spratova, navodeci "korpe" rucno
+
+hist(data$floor, breaks = seq(1, max(data$floor)))
+
+# Graficka tabela
+
+t2 = table(data$balcony, data$neighborhood)
+plot(t2)
+
+
+# Box plot cena i dela grada
+
+boxplot(data$price ~ data$neighborhood, data, xlab="neighborhood", ylab="price")
+
+# Pie chart po gradovima
+
+pie(table(data$neighborhood))
+
+
+# Ukljucimo ggplot2
+
+library(ggplot2)
+
+# Odnos tipa poda i lokacije u gradu
+
+ggplot(data, aes(x=neighborhood, fill=flooring_type)) + geom_bar(width = 0.5)
+
+ggplot(data, aes(x=neighborhood, fill=flooring_type)) 
+      + geom_bar(width = 0.5) 
+      + facet_wrap(~balcony)
+
+
